8b470a9175a60d84861858488d20aaed9063b13d
anderson1096 <anderson.mosquera9610@gmail.com>
2016-07-12 16:32:28 -0500

    Se agregaron imagenes y se añadió la detección facial para la GPU

---
+++ ComputerVision/FacialDetection/Haar.cpp
@@ -0,0 +1,57 @@
+#include "opencv2/core/core.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "iostream"
+#include <stdio.h>
+
+
+using namespace std;
+using namespace cv;
+
+CascadeClassifier face_cascade;
+string faces_source = "/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml";
+
+int main(int argc, char  **argv) {
+
+  
+
+
+  
+  vector<Rect> faces; //Vector para almacenar las caras detectadas
+  char *imageName = argv[1];
+
+  //Cargando el archivo xml para la detección de caras
+  if(!face_cascade.load(faces_source)){
+    cout << "Error : No se pudo cargar el archivo xml"<< endl;
+    return -1;
+  }
+
+  //Cargando la Imagen
+
+  Mat img = imread(imageName);
+  if(img.empty()){
+    cout << "Error: Imagen no cargada" << endl;
+    return -1;
+  }
+
+  //Se pasa la imagen (frame) a escala de grises, para calcular menos datos.
+  Mat gray;
+  cvtColor(img, gray, CV_BGR2GRAY);
+  equalizeHist(gray, gray);
+  //imshow("Gray", gray);
+
+  //Haciendo el procedimiento de detección facial
+  face_cascade.detectMultiScale(gray, faces, 1.2, 3);
+
+  //Pintando el rectangulo que señala la cara
+  for (int i = 0; i < int(faces.size()); i++) {
+    Rect r = faces[i];
+    rectangle(img, Point(r.x, r.y), Point(r.x + r.width, r.y + r.height), CV_RGB(0, 255, 0));
+  }
+  
+  imshow("Detection", img);
+  waitKey();
+
+  return 0;
+}
---
+++ ComputerVision/FacialDetection_GPU/Haar_GPU.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/contrib/contrib.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/gpu/gpu.hpp>
+#include <time.h>
+
+using namespace std;
+using namespace cv;
+using namespace cv::gpu;
+
+CascadeClassifier_GPU cascade_gpu;
+string faces_source = "/usr/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml";
+
+int main(int argc, char **argv) {
+
+
+  try{
+
+      clock_t start, end;
+      start = clock();
+      if(!cascade_gpu.load(faces_source)){
+        cout << "Error : No se pudo cargar el archivo xml" << endl;
+        return -1;
+      }
+      char *imageName = argv[1];
+      GpuMat faces;
+
+      //Cargando la imagen
+      Mat src_host = imread(imageName, 1);
+      GpuMat dst, src, gray;
+      if(argc != 2 || !src_host.data){
+        cout << "No Image Data" << endl;
+        return -1;
+      }
+
+      //Copiando imagen del host a la gpu
+      src.upload(src_host);
+
+      //convirtiendo a escala de grises en la gpu
+      cv::gpu::cvtColor(src, gray, CV_BGR2GRAY);
+      cv::gpu::equalizeHist(gray, gray);
+
+      //haciendo la detección facial en la gpu
+      int num_faces = cascade_gpu.detectMultiScale(gray, faces, 1.2, 3);
+
+
+      Mat obj_host;
+      if(num_faces != 0){
+         faces.colRange(0, num_faces).download(obj_host);
+         Rect *cfaces = obj_host.ptr<Rect>();
+      
+
+      // recuperando las caras y dibujando los rectangulos
+      for(int i = 0; i < num_faces; ++i) {
+         Point pt1 = cfaces[i].tl();
+         Size sz = cfaces[i].size();
+         Point pt2(pt1.x+sz.width, pt1.y+sz.height);
+         rectangle(src_host, pt1, pt2, CV_RGB(0, 255, 0));
+       }
+       }
+      cout << num_faces << endl;
+
+       end = clock();
+
+       double time_used = (end - start)/CLOCKS_PER_SEC;
+
+       //mostrando el tiempo
+       cout<< "Tiempo GPU : " << time_used << endl;
+
+       //Mostrando el resultado
+       imshow("Detection_GPU",src_host);
+       waitKey();
+
+     }
+
+     catch(const Exception& ex){
+       cout << "Error: " << ex.what() << endl;
+     }
+  return 0;
+}
